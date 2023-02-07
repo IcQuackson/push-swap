909edcd229611407301ab99b44d0987043c15ad0
IcQuackson <pedrorenato1998@gmail.com>
2023-02-07 00:33:48 +0000

    .

--- main.c
+++ main.c
@@ -3,30 +3,33 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pedgonca <pedgonca@student.42.fr>          +#+  +:+       +#+        */
+/*   By: quackson <quackson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 19:26:21 by pedgonca          #+#    #+#             */
-/*   Updated: 2023/02/02 17:22:05 by pedgonca         ###   ########.fr       */
+/*   Updated: 2023/02/07 00:11:08 by quackson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "operations.h"
+#include "stack_operations/stack_operations.h"
 #include "lst_operations/lst_operations.h"
+#include "smart_operations/smart_operations.h"
+#include "sorting_operations/sorting_operations.h"
+#include "others.h"
 
 #include <stdio.h>
 
 int	main(int argc, char **argv)
 {
-	t_list	*stack_a_head;
-	t_list	*stack_b_head;
+	t_list	*head_a;
+	t_list	*head_b;
 	int		i;
 
 	if (argc > 1)
 	{
-		stack_a_head = NULL;
-		stack_b_head = NULL;
+		head_a = NULL;
+		head_b = NULL;
 		i = 0;
-		if (!check_if_int_duplicate(argc, argv))
+		if (!check_arguments(argc, argv))
 		{
 			printf("Failed\n");
 			return (0);
@@ -38,90 +41,50 @@ int	main(int argc, char **argv)
 		} */
 		while (++i < argc)
 		{
-			if (!add_new_node(argv[i], &stack_a_head))
+			if (!add_new_node(argv[i], &head_a))
 			{
 				printf("Failed\n");
-				free_stack(stack_a_head);
+				free_stack(head_a);
 				return (0);
 			}
 		}
-		print_stacks(stack_a_head, stack_b_head);
-		// Swap a
-		/* swap(stack_a_head, 0, 1);
-		printf("swap a\n");
-		print_stacks(stack_a_head, stack_b_head);
-		swap(stack_a_head, 0, 1);
-		printf("swap a\n");
-		print_stacks(stack_a_head, stack_b_head); */
-		/* stack_a_head = swap(stack_a_head);
-		printf("swap a\n");
-		print_stacks(stack_a_head, stack_b_head); */
+		write(1, "---------------------------------------\n", 40);
+		write(1, "Init a and b:\n", 14);
+		print_stacks(head_a, head_b);
+		//sort(head_a, head_b);
 		// Push b
-		/* printf("push b\n");
-		push(&stack_a_head, &stack_b_head);
+		/* push_b(&stack_a_head, &stack_b_head);
 		print_stacks(stack_a_head, stack_b_head);
-		printf("push b\n");
-		push(&stack_a_head, &stack_b_head);
+		push_b(&stack_a_head, &stack_b_head);
 		print_stacks(stack_a_head, stack_b_head);
-		printf("push b\n");
-		push(&stack_a_head, &stack_b_head);
-		print_stacks(stack_a_head, stack_b_head);
+		push_b(&stack_a_head, &stack_b_head);
+		print_stacks(stack_a_head, stack_b_head); */
 		
 		// Push a
-		printf("push a\n");
-		push(&stack_b_head, &stack_a_head);
+		/* push_a(&stack_b_head, &stack_a_head);
 		print_stacks(stack_a_head, stack_b_head); */
-
-		// Get node
-		/* printf("Get first node\n");
-		t_list *node = get_node(stack_a_head, 0);
-		if (node)
-			printf("%d\n", node->content);
-		printf("Get last node\n");
-		node = get_node(stack_a_head, 4);
-		if (node)
-			printf("%d\n", node->content); */
-		// Rotate a
-		/* printf("Rotate a\n");
-		rotate(stack_a_head);
-		print_stacks(stack_a_head, stack_b_head);
-		printf("Rotate a\n");
-		rotate(stack_a_head);
-		print_stacks(stack_a_head, stack_b_head);
-		printf("Rotate a\n");
-		rotate(stack_a_head);
-		print_stacks(stack_a_head, stack_b_head); */
-		// Reverse Rotate a
-		/* printf("Reverse Rotate a\n");
-		reverse_rotate(stack_a_head);
-		print_stacks(stack_a_head, NULL);
 		
-		printf("Rotate a\n");
-		rotate(stack_a_head);
-		print_stacks(stack_a_head, NULL); */
+		/* rotate_a(stack_a_head);
+		print_stacks(stack_a_head, NULL);
 
-		/* printf("Push b 5 times\n");
-		push(&stack_a_head, &stack_b_head);
-		push(&stack_a_head, &stack_b_head);
-		push(&stack_a_head, &stack_b_head);
-		push(&stack_a_head, &stack_b_head);
-		push(&stack_a_head, &stack_b_head);
-		print_stacks(stack_a_head, stack_b_head); */
-
-		// Push b
-		/* printf("push b\n");
-		push(&stack_a_head, &stack_b_head);
-		print_stacks(stack_a_head, stack_b_head); */
-		/* printf("Swap a and b\n");
-		double_swap(stack_a_head, stack_b_head);
+		rotate_ab(stack_a_head, stack_b_head);
 		print_stacks(stack_a_head, stack_b_head);
 
-		printf("Double Rotate\n");
-		double_rotate(stack_a_head, stack_b_head);
-		print_stacks(stack_a_head, stack_b_head);
-
-		printf("Double Reverse Rotate\n");
-		double_reverse_rotate(stack_a_head, stack_b_head);
+		reverse_rotate_ab(stack_a_head, stack_b_head);
 		print_stacks(stack_a_head, stack_b_head); */
+
+		/* printf("max index: %d\n", get_index_of_max_value(stack_a_head));
+		printf("min index: %d\n", get_index_of_min_value(stack_a_head)); */
+		
+		/* get_lst_to_top(stack_a_head, argc - 1, 3, rotate_a, reverse_rotate_a);
+		write(1, "---------------------------------------\n", 40);
+		print_stacks(stack_a_head, stack_b_head); */
+
+		//get_lst_to_bot(head_a, 1, rotate_a, reverse_rotate_a);
+		write(1, "---------------------------------------\n", 40);
+		sort(head_a, head_b);
+		write(1, "---------------------------------------\n", 40);
+		print_stacks(head_a, NULL);
 	}
 }
+
